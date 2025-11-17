da5f9049dd9bd7b7974f532d67f529373d9b6a7c
classjiy <class.jiy@gmail.com>
2025-11-17 10:25:00 +0900

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCounter] = useState(0);
+
+  const minus10 = () => {
+    setCounter(count - 10);
+  }
+
+  const minus1 = () => {
+    setCounter(count - 1);
+  }
+
+  const plus1 = () => {
+    setCounter(count + 1);
+  }
+
+  const plus10 = () => {
+    setCounter(count + 10);
+  }
+
+  const reset = () => {
+    setCounter(0);
+  }
+
+  return (
+    <>
+      <div id="wrap">
+        <div className="inner">
+          <h1>Simple Counter</h1>
+          <p className="number">{count}</p>
+          <div className="btn">
+            <button onClick={minus10}>-10</button>
+            <button onClick={minus1}>-1</button>
+            <button onClick={reset} className='reset'>RESET</button>
+            <button onClick={plus1}>+1</button>
+            <button onClick={plus10}>+10</button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
